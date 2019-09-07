8473cc856d56abbe43709daa2bd593a736c60b4f
Dival Banerjee <bandiv2011@gmail.com>
2019-09-07 12:35:26 -0400

    Added csv functionality

--- src/battery/cell.py
+++ src/battery/cell.py
@@ -1,68 +1,72 @@
+import csv
+impoort cell.py
 
-class cell:
-    def __init__(self, ratedVoltage, maxAmperage):
-        self.cellName = 'generic cell'
-        self.intitialPotential = -1
-        self.finalPotential = -1
-        self.ratedPotential = ratedVoltage
-        self.capacity = -1
-        self.maxDischarge = maxAmperage
-        self.internalResistance = -1
-        self.weight = -1
-    
-    def setCellName(self, name):
-        self.cellName = name
 
-    def setInitialPotential(self, volts):
-        self.intitialPotential = volts
-    
-    def setFinalPotential(self, volts):
-        self.finalPotential = volts
-    
-    def setRatedPotential(self, volts):
-        self.ratedPotential = volts
-    
-    def setCapacity(self, ampHours):
-        self.setCapacity = ampHours
-    
-    def setMaxDischarge(self, amps):
-        self.maxDischarge = amps
-    
-    def setInternalResistance(self,ohms):
-        self.internalResistance = ohms
-    
-    def setWeight(self, grams):
-        self.weight = grams
+class pack:
 
-    def getCellName(self):
-        return self.cellName
-    
-    def getInitialPotential(self):
-        return self.intitialPotential
-    
-    def getFinalPotential(self):
-        return self.finalPotential
-    
-    def getMaxDischarge(self):
-        return self.maxDischarge
+        cellsInParallel = 0
+        cellsInSeries = 0
+        energyRequired = 0
+        voltageRequired = 0
+        powerRequired = 0
+        additionalCapacity = 30
+        cell = cell(0,0)
+        cellList[]
 
-    def getCapacity(self):
-        return self.capacity
+    def __init__(self):
+        self.cellsInParallel = 0
+        self.cellsInSeries = 0
+        self.energyRequired = 0
+        self.voltageRequired = 0
+        self.powerRequired = 0
+        self.additionalCapacity = 30
+        self.cell = cell(0,0)
 
-    def findEnergyDensity(self):
-        energyDensity = self.capacity/self.weight
-        return energyDensity
+    def setEnergyRequired(self, energy):
+        self.energyRequired = energy
+    
+    def setVoltageRequired(self, voltage):
+        self.voltageRequired = voltage
 
-    def findPowerDensity(self):
-        #TODO: This will have to be done later, the discharge curve needs to be found with polynomial approximation
+    def setPowerRequired(self,power):
+        self.powerRequired = power
+        
+    def findPackConfig(self):
+        self.cellsInSeries = voltageReqired/self.cell.getVoltage()
+        self.cellsForCapacity = (self.energyRequired/((self.cell.getCapacity()-.7))*1.3 
+        self.cellsForPower  = self.powerRequired/self.cell.getMaxDischarge()
+    
+
+    def loadCellInfo(self,path):
+        with open('cellPortfolio.txt') as csv_file:
+            csvReader = csv.reader(csv_file, delimiter=';')
+            lineCount = 0
+            for row in csvReader:
+                if lineCount == 0:
+                    #First line
+                    #print(f'Column names are {", ".join(row)}')
+
+                    lineCount += 1
+                else:
+                    #line 2 and after
+                    #print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+                    cell
+                    lineCount += 1
+                    
+
+
+    def findCellsRequiredForPower(self):
         
 
-    def findUsableCapacity(self):
-        #TODO: This will have to be done later, the discharge curve needs to be found with polynomial approximation
+    def findCellsRequiredForVoltage(self):
         
 
-    def findUsableEnergyDensity:
-        #TODO: This will have to be done later, the discharge curve needs to be found with polynomial approximation
+    def optimizePack(self):
+        #Optimize pack for weight
 
     
+
         
+
+    
+    
