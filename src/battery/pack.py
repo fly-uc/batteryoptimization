4b91fb9ed2c81bb4d0af73eb6dd181a95c0ffa27
Dival Banerjee <banerjdl@ceas1.uc.edu>
2019-09-07 17:21:31 -0400

    most errors fixed

--- src/battery/pack.py
+++ src/battery/pack.py
@@ -1,19 +1,20 @@
 
 
 import csv
-impoort cell.py
+import cell
 
+class pack(object):
 
-class pack:
-
-        cellsInParallel = 0
-        cellsInSeries = 0
-        energyRequired = 0
-        voltageRequired = 0
-        powerRequired = 0
-        additionalCapacity = 30
-        cell = cell(0,0)
-        cellList[]
+    cellsInParallel = 0
+    cellsInSeries = 0
+    energyRequired = 0
+    voltageRequired = 0
+    powerRequired = 0
+    additionalCapacity = 30
+    totalCells = 0
+    weightInKilograms = 0
+    cell = cell(0,0)
+    cellList = []
 
     def __init__(self):
         self.cellsInParallel = 0
@@ -33,35 +34,42 @@ class pack:
     def setPowerRequired(self,power):
         self.powerRequired = power
         
-    def findPackConfig(self):
-        self.cellsInSeries = voltageReqired/self.cell.getVoltage()
-        self.cellsForCapacity = (self.energyRequired/((self.cell.getCapacity()-.7))*1.3 
+    #rough calculation from earlier
+    def findBasicPackConfig(self,cell):
+        self.cellsInSeries = self.voltageReqired/self.cell.getVoltage()
+        self.cellsForCapacity = (self.energyRequired/((self.cell.getCapacity()-.7)))*1.3 
         self.cellsForPower  = self.powerRequired/self.cell.getMaxDischarge()
     
 
     def loadCellInfo(self,path):
-        with open('cellPortfolio.txt') as csv_file:
-            csvReader = csv.reader(csv_file, delimiter=';')
+        with open('cellList.txt') as csvFile:
+            csvReader = csv.reader(csvFile, delimiter=';')
             lineCount = 0
             for row in csvReader:
                 if lineCount == 0:
                     #First line
-                    #print(f'Column names are {", ".join(row)}')
-
+                    print ('Cell Portfolio')
                     lineCount += 1
                 else:
                     #line 2 and after
-                    #print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-                    cell
+                    newCell = cell(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8])
+                    cellList.append(newCell)
+                    print (newCell.toString())
                     lineCount += 1
-                    
-
-
+                
     def findCellsRequiredForPower(self, cell):
         
 
-    def findCellsRequiredForVoltage(self,cell):
-        
+    def findCellsRequiredForVoltage(self, cell):
+
+    def findTotalCells(self):
+        self.totalCells = self.cellsInParallel * self.cellsInSeries
+
+    def findWeight(self):
+        self.weightInKilograms = self.totalCells
+
+    def findThermalLosses(self):
+
 
     def optimizePack(self):
         #Optimize pack for weight
