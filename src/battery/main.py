042b6fcc68c246600dd95b6bcb7facdd05f68992
Keerthi Sekar <sekarku@mail.uc.edu>
2019-09-10 22:21:36 -0400

    Update main.py
    Need to fix parsing errors

--- src/battery/main.py
+++ src/battery/main.py
@@ -1,28 +1,17 @@
 import csv
 import cell
 #TODO: get motor import to work
-class Test(object):
-    def __init__(self, t1, t2, t3):
-        self.col1 = t1
-        self.col2 = t2
-        self.col3 = t3
 
-def loadCSVIntoArray(path):
-    objList = []
+def loadCSVIntoArray(path, objList):
     with open(path) as csvFile:
         csvReader = csv.reader(csvFile, delimiter=';')
         lineCount = 0
         for row in csvReader:
-            if lineCount == 1:
-                newTest = Test(row[0],row[1],row[2])
-                objList.append(newTest)
-                print (newTest.toString())   
-            lineCount += 1
-    return objList
+            objList.append(cell(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8]))
 
 def main():
-    motors = []
-    motors = loadCSVIntoArray('C:/Users/Keerthi Sekar/Documents/GitHub/batteryoptimization/src/battery/motorTest.csv')
-    print(motors)
+    cells = []
+    loadCSVIntoArray('C:/Users/Keerthi Sekar/Documents/GitHub/batteryoptimization/src/battery/cellList.txt', cells)
+    print(cells)
 
 main()
