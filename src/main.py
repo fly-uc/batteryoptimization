179975b46cbbed6589d294de7d03cd3306374bdc
Dival Banerjee <bandiv2011@gmail.com>
2020-08-13 17:41:40 -0400

    Added parameters for main
    added parameters for main

--- src/main.py
+++ src/main.py
@@ -4,3 +4,14 @@ import src.cell
 import src.motor
 import src.pack
 
+#Vehicle inputs:
+motorCount = 4
+#example
+#Motor input:
+motorName = 'U7 V2.0 - KV 420'
+motorVoltage =  25
+motorMaxCurrent = 47.5
+
+#Energy input:
+#Format [power(Watts), duration(hours)],[power2(Watts), duration2(hours)]
+powerInterval = [[8650,.00833],[5970,.5], [7040, .5], [3830,.00833]]
