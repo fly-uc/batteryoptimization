ac4f28df9e1892232ac864c473a2151594d8b74e
Dival Banerjee <bandiv2011@gmail.com>
2020-08-13 17:41:22 -0400

    Created variables for cell class
    created variables for cell class

--- src/cell.py
+++ src/cell.py
@@ -1,1 +1,12 @@
 #file --cell.py--
+
+class cell(object):
+    name = "empty"
+    initialVoltage = -1
+    finalVoltage = -1
+    ratedVoltage = -1
+    capacity = -1
+    maxContinousDischarge = -1
+    internalResistance = -1
+    remainingCapacity = -1 #TODO: Rename this
+    usableCapacity = -1
