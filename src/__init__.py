c5cd29f5caf6b134ca79a31235a03aef7841a1ac
Dival Banerjee <bandiv2011@gmail.com>
2020-10-08 18:14:29 -0400

    Added to init.py, doesn't need python path variable set

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+from .cell import *
+from .CellTable import *
+from .motor import *
+from .pack import *
+from .vehicle import *
+from .main import *
--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 #file --main.py--
 
-import src.cell
-import src.motor
-import src.pack
-import src.vehicle
+from src.cell import cell
+from src.motor import motor
+from src.pack import pack
+from src.vehicle import vehicle
 
 #Vehicle inputs:
 motorCount = 4
@@ -16,3 +16,10 @@ motorMaxCurrent = 47.5
 #Energy input:
 #Format [power(Watts), duration(hours)],[power2(Watts), duration2(hours)]
 powerInterval = [[8650,.00833],[5970,.5], [7040, .5], [3830,.00833]]
+
+myPack = pack()
+myPack.setVoltageRequired(motorVoltage)
+myPack.setPowerRequried(motorCount*motorMaxCurrent)
+myPack.energyRequiredFromList(powerInterval)
+print('_______________________________________________________________')
+myPack.optimizePack()
